5a72912b3ccbebc0742d8ae82989d14da4a4e345
Celina Jackson <65145705+CelinaJackson@users.noreply.github.com>
2021-01-15 14:22:56 -0500

    Update school_classes_controller.rb
    refactoring to use (*args)

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -12,7 +12,7 @@ class SchoolClassesController < ApplicationController
     end 
 
     def create 
-        @school_class = SchoolClass.new(school_class_params)
+        @school_class = SchoolClass.new(school_class_params[:title][:room_number])
         @school_class.save 
         redirect_to school_class_path(@school_class)
     end
@@ -23,13 +23,13 @@ class SchoolClassesController < ApplicationController
 
     def update 
         @school_class = SchoolClass.find(params[:id])
-        @school_class.update(school_class_params)
+        @school_class.update(school_class_params[:room_number])
         redirect_to school_class_path(@school_class)
     end 
 
     private 
 
-    def school_class_params
-        params.require(:school_class).permit(:title, :room_number)
+    def school_class_params(*args)
+        params.require(:school_class).permit(*args)
     end 
 end 
